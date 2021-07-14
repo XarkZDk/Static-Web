d36ecc9ac510d8644bfc1b902ee6d7d21f186e3c
XarkZ <diegoxken@gmail.com>
2021-07-13 22:32:43 -0400

    v2

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ app.set('appName','StaticWeb')
 app.set('port',3000)
 
 app.use(morgan('dev'))
-app.use(express.static('public'))
 
 app.get('*',(req,res)=>{ // "*" -> Cuando la ruta no existe 
     res.sendFile(__dirname + "/404.html")
 })
+
+app.use(express.static('public'))
 
 app.listen(app.get('port'),()=>{
     console.log(app.get('appName'))
